70cf442cdc644c9aa80e5a33b0e7b47d7fd42840
Vincent <vinh.nglx@gmail.com>
2017-03-20 23:23:40 +0800

    Coverall configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,9 @@ require 'rspec/rails'
 require 'simplecov'
 SimpleCov.start
 
+# Coveralls configuration
+Coveralls.wear!
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
