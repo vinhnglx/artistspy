7c13c937e01f666689b49ab45fb5228673c941bd
Vincent <vinh.nglx@gmail.com>
2017-03-21 17:30:50 +0800

    Display the error message incase we type wrong URLs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  # Public: Display the error with wrong URL
+  #
+  # Example
+  #
+  #   get('/api2/23482039/23209')
+  #   # => { errors: [ { status: 400, title: 'Bad Request' } ] }
+  #
+  # Returns the JSON error message
+  def routing_error
+    render json: {
+      errors: [
+        {
+          status: 400,
+          title: 'Bad Request'
+        }
+      ]
+    }, status: 400
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
       resources :artists, only: :index
     end
   end
+
+  # Routes to handle input wrong API endpoints
+  get '*path', to: 'application#routing_error'
+
+  root "home#index"
 end
