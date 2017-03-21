61ca3dd48596c256f09807510dcea49be7c260e6
Vincent <vinh.nglx@gmail.com>
2017-03-21 18:42:58 +0800

    Add one more test incase we try to favourite an artist twice

--- spec/controllers/api/v1/favourites_controller_spec.rb
+++ spec/controllers/api/v1/favourites_controller_spec.rb
@@ -22,11 +22,26 @@ RSpec.describe Api::V1::FavouritesController, type: :controller do
         @request.headers["Content-Type"] = 'application/vnd.api+json'
         post :create, params: @data
         expect(response).to have_http_status(:created)
+        expect(Artist.count).to eq 1
       end
 
       it 'returns not_acceptable status when we forget passing the Content-Type header' do
         post :create, params: @data
         expect(response).to have_http_status(:not_acceptable)
+      end
+
+      it 'returns an error when we try to favourite an artist twice' do
+        create(:artist,
+          external_urls: 'https://open.spotify.com/artist/7dGJo4pcD2V6oG8kP0tJRR',
+          genres: ['hiphop', 'rap'],
+          href: "https://api.spotify.com/v1/artists/7dGJo4pcD2V6oG8kP0tJRR",
+          spotify_id: '7dGJo4pcD2V6oG8kP0tJRR',
+          name: 'Eminem'
+        )
+
+        @request.headers["Content-Type"] = 'application/vnd.api+json'
+        post :create, params: @data
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
