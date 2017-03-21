a31e5d64ef50534d378b22338f265ed63cf6487d
Vincent <vinh.nglx@gmail.com>
2017-03-21 16:09:20 +0800

    Update ArtistServiceSpec after change on SearchableService

--- spec/services/artist_service_spec.rb
+++ spec/services/artist_service_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ArtistService do
-  let!(:searchable) { SearchableService.new('/v1/search?type=artist', 50, 'eminem') }
+  let!(:searchable) { SearchableService.new(endpoint: '/v1/search?type=artist', limit: 50, query: 'eminem') }
   let!(:artists_service) { ArtistService.new(searchable) }
 
-  it 'returns the formatted artists collection' do
+  it 'returns the formatted artists collection containing attributes' do
     fields = [:external_urls, :genres, :href, :name, :spotify_id]
     artists = artists_service.transform
 
