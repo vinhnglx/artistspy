524ff7e8d65b966201427237792c1a9dedbf8381
Vincent <vinh.nglx@gmail.com>
2017-03-21 14:58:00 +0800

    Change spec text for SearchableService

--- spec/services/searchable_service_spec.rb
+++ spec/services/searchable_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe SearchableService do
   let!(:searchable) { SearchableService.new('/v1/search?type=artist', 50, 'eminem') }
 
-  it 'returns JSON artists' do
+  it 'returns the JSON artists' do
     response = searchable.connect
     jdata = JSON.parse(response.body)
     expect(response.code).to eq 200
